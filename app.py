8eb19816505e026b8d5c0cb25e0f4fc63c019234
Colton McLaughlin <cjr2d29719@gmail.com>
2024-04-30 12:28:59 -0600

    Reproducing issue.

--- app.py
+++ app.py
@@ -3,26 +3,18 @@ import os
 
 import aws_cdk as cdk
 
-from issue.issue_stack import IssueStack
+from issue.issue_stack import IssueStack2
 
 
 app = cdk.App()
-IssueStack(app, "IssueStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
+issue_stack = IssueStack2(
+    app,
+    "IssueStack2",
+)
 
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
 
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+# deploy the stack, then uncomment the line below and the deployment will fail
+# cdk.Tags.of(issue_stack).add("Project", "ethics")
 
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
-
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
 
 app.synth()
--- issue/issue_stack.py
+++ issue/issue_stack.py
@@ -1,19 +1,29 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+)
+from aws_cdk import aws_lambda as lambda_
+from aws_cdk import (
+    aws_signer as signer,
 )
 from constructs import Construct
 
-class IssueStack(Stack):
 
+class IssueStack2(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        signing_profile = signer.SigningProfile(
+            self,
+            "Signing Profile",
+            platform=signer.Platform.AWS_LAMBDA_SHA384_ECDSA,
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "IssueQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        code_signing_config = lambda_.CodeSigningConfig(
+            self,
+            "Code Signing Config",
+            signing_profiles=[
+                signing_profile,
+            ],
+            description=f"CDK Signing Config for {Stack.of(self).stack_name}",
+            untrusted_artifact_on_deployment=lambda_.UntrustedArtifactOnDeployment.WARN,
+        )
